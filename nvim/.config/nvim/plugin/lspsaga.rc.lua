b52dc0dcc3e849add13046737201185a8b18a93a
Umut Topuzoğlu <uloco@schiefewelt.de>
2022-09-12 21:54:02 +0200

    Configure lspsaga

--- nvim/.config/nvim/plugin/lspsaga.rc.lua
+++ nvim/.config/nvim/plugin/lspsaga.rc.lua
@@ -2,105 +2,44 @@ local status, saga = pcall(require, 'lspsaga')
 if (not status) then return end
 
 saga.init_lsp_saga {
-  server_filetype_map = {
-    typescript = 'typescript'
+  border_style = "rounded",
+  saga_winblend = 15,
+  -- when cursor in saga window you config these to move
+  move_in_saga = { prev = '<C-p>', next = '<C-n>' },
+  diagnostic_header = { " ", " ", " ", "ﴞ " },
+  -- preview lines of lsp_finder and definition preview
+  max_preview_lines = 50,
+  code_action_icon = "",
+  code_action_lightbulb = {
+    sign = false,
+    virtual_text = true,
   },
   finder_action_keys = {
+    open = "<CR>",
+    vsplit = "v",
+    split = "s",
     quit = "<esc>",
-    exec = '<CR>',
   },
   code_action_keys = {
     quit = "<esc>",
+    exec = "<CR>",
   },
   definition_action_keys = {
-    edit = '<CR>',
-    vsplit = '<C-v>',
-    split = '<C-s>',
-    quit = '<esc>',
+    edit = '<CRY',
+    vsplit = '<C-c>v',
+    split = '<C-c>i',
+    tabe = '<C-c>t',
+    quit = "<esc>",
+  },
+  scroll_in_preview = {
+    scroll_down = '<C-d>',
+    scroll_up = '<C-u>',
   },
   rename_action_quit = "<esc>",
 
-
-
-  -- Options with default value
-  -- "single" | "double" | "rounded" | "bold" | "plus"
-  border_style = "rounded",
-  --the range of 0 for fully opaque window (disabled) to 100 for fully
-  --transparent background. Values between 0-30 are typically most useful.
-  -- saga_winblend = 0,
-  -- -- when cursor in saga window you config these to move
-  -- move_in_saga = { prev = '<C-p>', next = '<C-n>' },
-  -- -- Error, Warn, Info, Hint
-  -- -- use emoji like
-  -- -- { "🙀", "😿", "😾", "😺" }
-  -- -- or
-  -- -- { "😡", "😥", "😤", "😐" }
-  -- -- and diagnostic_header can be a function type
-  -- -- must return a string and when diagnostvimic_header
-  -- -- is function type it will have a param `entry`
-  -- -- entry is a table type has these filed
-  -- -- { bufnr, code, col, end_col, end_lnum, lnum, message, severity, source }
-  -- diagnostic_header = { " ", " ", " ", "ﴞ " },
-  -- -- preview lines of lsp_finder and definition preview
-  -- max_preview_lines = 10,
-  -- -- use emoji lightbulb in default
-  -- code_action_icon = "💡",
-  -- -- if true can press number to execute the codeaction in codeaction window
-  -- code_action_num_shortcut = true,
-  -- -- same as nvim-lightbulb but async
-  -- code_action_lightbulb = {
-  --   enable = true,
-  --   enable_in_insert = true,
-  --   cache_code_action = true,
-  --   sign = true,
-  --   update_time = 150,
-  --   sign_priority = 20,
-  --   virtual_text = true,
-  -- },
-  -- -- finder icons
-  -- finder_icons = {
-  --   def = '  ',
-  --   ref = '諭 ',
-  --   link = '  ',
-  -- },
-  -- -- finder do lsp request timeout
-  -- -- if your project big enough or your server very slow
-  -- -- you may need to increase this value
-  -- finder_request_timeout = 1500,
-  -- -- set antoher colorscheme in preview window
-  -- finder_preview_hl_ns = 0,
-  -- ,
-  -- rename_in_select = true,
-  -- -- show symbols in winbar must nightly
-  -- symbol_in_winbar = {
-  --   in_custom = false,
-  --   enable = false,
-  --   separator = ' ',
-  --   show_file = true,
-  --   click_support = false,
-  -- },
-  -- -- show outline
-  -- show_outline = {
-  --   win_position = 'right',
-  --   --set special filetype win that outline window split.like NvimTree neotree
-  --   -- defx, db_ui
-  --   win_with = '',
-  --   win_width = 30,
-  --   auto_enter = true,
-  --   auto_preview = true,
-  --   virt_text = '┃',
-  --   jump_key = 'o',
-  --   -- auto refresh when change buffer
-  --   auto_refresh = true,
-  -- },
-  -- -- custom lsp kind
-  -- -- usage { Field = 'color code'} or {Field = {your icon, your color code}}
-  -- custom_kind = {},
-  -- -- if you don't use nvim-lspconfig you must pass your server name and
-  -- -- the related filetypes into this table
-  -- -- like server_filetype_map = { metals = { "sbt", "scala" } }
-
+  server_filetype_map = {},
 }
+
 
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<leader>jp', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
@@ -111,3 +50,4 @@ vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
 vim.keymap.set('n', '<F2>', '<Cmd>Lspsaga rename<CR>', opts)
 vim.keymap.set('n', '<Leader>ca', '<Cmd>Lspsaga code_action<CR>', opts)
 vim.keymap.set('v', '<Leader>ca', '<Cmd>Lspsaga range_code_action<CR>', opts)
+vim.keymap.set('v', '<Leader>tt', '<Cmd>Lspsaga range_code_action<CR>', opts)
