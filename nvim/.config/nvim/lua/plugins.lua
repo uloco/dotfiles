c9b9b9983318d9cec64677c37843e6431962c5de
Umut Topuzoğlu <uloco@schiefewelt.de>
2022-09-14 00:21:58 +0200

    Remove buggy tree view

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -104,11 +104,4 @@ packer.startup(function(use)
     disabled = true
   }
 
-  -- tree view explorer
-  use {
-    'kyazdani42/nvim-tree.lua',
-    requires = {
-      'kyazdani42/nvim-web-devicons', -- optional, for file icons
-    },
-  }
 end)
