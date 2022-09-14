d19466b3a93b59097b9365594ce7e24f3b009ce4
Umut Topuzoğlu <uloco@schiefewelt.de>
2022-09-14 22:58:20 +0200

    Add organize imports on save

--- nvim/.config/nvim/after/plugin/null-ls.rc.lua
+++ nvim/.config/nvim/after/plugin/null-ls.rc.lua
@@ -3,6 +3,15 @@ if (not status) then return end
 
 local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
 
+local function organize_imports()
+  local params = {
+    command = "_typescript.organizeImports",
+    arguments = { vim.api.nvim_buf_get_name(0) },
+    title = ""
+  }
+  vim.lsp.buf.execute_command(params)
+end
+
 ---@diagnostic disable-next-line: redundant-parameter
 null_ls.setup {
   sources = {
@@ -13,11 +22,15 @@ null_ls.setup {
   },
   on_attach = function(client, bufnr)
     if client.server_capabilities.documentFormattingProvider then
+      -- auto format + organize imports
       vim.api.nvim_clear_autocmds { buffer = 0, group = augroup_format }
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = augroup_format,
         buffer = 0,
-        callback = function() vim.lsp.buf.formatting_seq_sync() end
+        callback = function()
+          organize_imports()
+          vim.lsp.buf.formatting_seq_sync()
+        end
       })
     end
   end,
--- nvim/.config/nvim/plugin/lspconfig.rc.lua
+++ nvim/.config/nvim/plugin/lspconfig.rc.lua
@@ -3,20 +3,25 @@ if (not status) then return end
 
 local protocol = require('vim.lsp.protocol')
 
-local on_attach = function(client, bufnr)
-  -- formatting
-  if client.server_capabilities.documentFormattingProvider then
-    vim.api.nvim_command [[augroup Format]]
-    vim.api.nvim_command [[autocmd! * <buffer>]]
-    vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
-    vim.api.nvim_command [[augroup END]]
-  end
+function organize_imports()
+  local params = {
+    command = "_typescript.organizeImports",
+    arguments = { vim.api.nvim_buf_get_name(0) },
+    title = ""
+  }
+  vim.lsp.buf.execute_command(params)
 end
 
 nvim_lsp.tsserver.setup {
   on_attach = on_attach,
   filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
-  cmd = { "typescript-language-server", "--stdio" }
+  cmd = { "typescript-language-server", "--stdio" },
+  commands = {
+    OrganizeImports = {
+      organize_imports,
+      description = "Organize Imports"
+    }
+  }
 }
 
 nvim_lsp.sumneko_lua.setup {
