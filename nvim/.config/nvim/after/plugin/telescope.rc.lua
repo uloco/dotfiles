f420460f63c2e3dacd33bf4da448d3171fabd304
Umut Topuzoğlu <uloco@schiefewelt.de>
2022-09-17 02:40:20 +0200

    Fix all git folders ignored in file browser

--- nvim/.config/nvim/after/plugin/telescope.rc.lua
+++ nvim/.config/nvim/after/plugin/telescope.rc.lua
@@ -26,9 +26,7 @@ telescope.setup {
       }
     },
     file_ignore_patterns = {
-      ".git/",
-      "node_modules/",
-      "Pods/",
+      "^.git/",
       ".DS_Store",
       ".vimundo",
     },
@@ -138,6 +136,7 @@ vim.keymap.set('n', '<leader>fb',
     telescope.extensions.file_browser.file_browser({
       path = "%:p:h",
       cwd = telescope_buffer_dir(),
+      file_ignore_patterns = {},
       respect_gitignore = false,
     })
   end,
