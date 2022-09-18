cf033e6b040ab072b0353866ada279742c9247d7
Umut Topuzoğlu <uloco@schiefewelt.de>
2022-09-18 21:50:40 +0200

    Cleanup

--- nvim/.config/nvim/after/plugin/toggleterm.rc.lua
+++ nvim/.config/nvim/after/plugin/toggleterm.rc.lua
@@ -3,7 +3,7 @@ if (not status) then return end
 
 toggleterm.setup({
   hide_numbers = false,
-  open_mapping = '<A-ß>',
+  open_mapping = '†', -- Alt-Gr + t
   insert_mappings = true,
   terminal_mappings = true,
   direction = 'float',
@@ -22,8 +22,8 @@ local lazygit  = Terminal:new({ cmd = "source ~/.zshrc; lazygit", hidden = true 
 local htop     = Terminal:new({ cmd = "htop", hidden = true })
 
 
-vim.keymap.set({ "n", "t" }, "©", function() lazygit:toggle() end, { noremap = true, silent = true })
-vim.keymap.set({ "n", "t" }, "ª", function() htop:toggle() end, { noremap = true, silent = true })
+vim.keymap.set({ "n", "t" }, "©", function() lazygit:toggle() end, { noremap = true, silent = true }) -- Alt-Gr + g
+vim.keymap.set({ "n", "t" }, "ª", function() htop:toggle() end, { noremap = true, silent = true }) -- Alt-Gr + h
 
 local cmd = vim.api.nvim_create_user_command
 cmd("LazygitOpen", function() lazygit:open() end, {})
