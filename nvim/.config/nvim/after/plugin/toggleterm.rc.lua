74f155d2ade04b5721947b804b59de932a138c67
Umut Topuzoğlu <uloco@schiefewelt.de>
2022-09-23 21:24:40 +0200

    Speedup lazygit open

--- nvim/.config/nvim/after/plugin/toggleterm.rc.lua
+++ nvim/.config/nvim/after/plugin/toggleterm.rc.lua
@@ -18,15 +18,27 @@ toggleterm.setup({
   }
 })
 
-vim.keymap.set({ "n", "t" }, "˝", "<cmd>ToggleTermToggleAll<cr>", { noremap = true, silent = true }) -- Alt-Gr + shift + t
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set({ "n", "t" }, "˝", "<cmd>ToggleTermToggleAll<cr>", opts) -- Alt-Gr + shift + t
 
 local Terminal = require('toggleterm.terminal').Terminal
-local lazygit  = Terminal:new({ cmd = "source ~/.zshrc; lazygit", hidden = true, direction = 'float' })
-local htop     = Terminal:new({ cmd = "htop", hidden = true, direction = 'float' })
+local lazygit  = Terminal:new({
+  cmd = [[
+    THEME=$(defaults read -globalDomain AppleInterfaceStyle &> /dev/null && echo dark || echo light)
+    XDG_CONFIG_DIR=${HOME}/.config
+    CONFIG_BASE=${XDG_CONFIG_DIR}/lazygit/config.yml
+    CONFIG_THEME=${XDG_CONFIG_DIR}/lazygit/config-${THEME}.yml
+    lazygit --use-config-file=${CONFIG_BASE},${CONFIG_THEME}
+    ]],
+  hidden = true,
+  direction = 'float',
+})
 
+local htop = Terminal:new({ cmd = "htop", hidden = true, direction = 'float' })
 
-vim.keymap.set({ "n", "t" }, "©", function() lazygit:toggle()  vim.cmd('checktime') end, { noremap = true, silent = true }) -- Alt-Gr + g
-vim.keymap.set({ "n", "t" }, "ª", function() htop:toggle() end, { noremap = true, silent = true }) -- Alt-Gr + h
+vim.keymap.set({ "n", "t" }, "©", function() lazygit:toggle() vim.cmd('checktime') end, opts)
+vim.keymap.set({ "n", "t" }, "ª", function() htop:toggle() end, opts) -- Alt-Gr + h
 
 local cmd = vim.api.nvim_create_user_command
 cmd("LazygitOpen", function() lazygit:open() end, {})
