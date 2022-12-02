cbca3773feda51c28f2a995d72ba1ee6bbeb5d1a
Umut Topuzoğlu <uloco@schiefewelt.de>
2022-12-02 16:11:18 +0100

    Add numbers for nvim tree

--- nvim/.config/nvim/after/plugin/tree.rc.lua
+++ nvim/.config/nvim/after/plugin/tree.rc.lua
@@ -1,7 +1,12 @@
 local status, tree = pcall(require, 'nvim-tree')
 if (not status) then return end
 
-tree.setup()
+tree.setup({
+  view = {
+    number = true,
+    relativenumber = true
+  }
+})
 
 vim.keymap.set('n', '<leader>b', '<cmd>NvimTreeToggle<cr>')
 vim.keymap.set('n', '<leader>ff', '<cmd>NvimTreeFindFile<cr>')
