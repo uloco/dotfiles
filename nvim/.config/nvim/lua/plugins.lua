ff795f54783080aae0d22efb8c3ef8739a341ec2
Umut Topuzoğlu <uloco@schiefewelt.de>
2022-12-04 23:32:43 +0100

    cleanup

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -181,12 +181,9 @@ packer.startup(function(use)
   }
 
   -- markdown preview
-  use({
+  use {
     "iamcco/markdown-preview.nvim",
     run = function() vim.fn["mkdp#util#install"]() end,
-  })
-
-  use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install",
-    setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+  }
 
 end)
