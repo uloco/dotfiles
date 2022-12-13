554038cc2d20f6813a39f53060de0f4341987e6b
Umut Topuzoğlu <uloco@schiefewelt.de>
2022-12-13 12:24:05 +0100

    Add utils

---
+++ nvim/.config/nvim/lua/utils.lua
@@ -0,0 +1,16 @@
+-- Print contents of `tbl`, with indentation.
+-- `indent` sets the initial level of indentation.
+function tprint(tbl, indent)
+  if not indent then indent = 0 end
+  for k, v in pairs(tbl) do
+    formatting = string.rep("  ", indent) .. k .. ": "
+    if type(v) == "table" then
+      print(formatting)
+      tprint(v, indent + 1)
+    elseif type(v) == 'function' or type(v) == 'boolean' then
+      print(formatting .. tostring(v))
+    else
+      print(formatting .. v)
+    end
+  end
+end
