06ea1e00a14a8e24d969024246e18f66e93eeb94
Umut Topuzoğlu <uloco@schiefewelt.de>
2023-02-08 14:45:02 +0100

    Don't search todos in e2e folder

--- nvim/.config/nvim/after/plugin/todo-comments.rc.lua
+++ nvim/.config/nvim/after/plugin/todo-comments.rc.lua
@@ -1,7 +1,23 @@
 local status, todo = pcall(require, "todo-comments")
 if (not status) then return end
 
-todo.setup()
+todo.setup({
+  search = {
+    command = "rg",
+    args = {
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--glob=!e2e"
+    },
+    -- regex that will be used to match keywords.
+    -- don't replace the (KEYWORDS) placeholder
+    pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+    -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+  },
+})
 
 vim.keymap.set("n", "<leader>jt", function()
   require("todo-comments").jump_next()
