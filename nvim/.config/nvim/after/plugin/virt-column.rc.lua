947b76594ae76071adace53b18e6f18540968236
Umut Topuzoğlu <uloco@schiefewelt.de>
2023-06-29 10:38:05 +0200

    Fix virt column in toggle term

--- nvim/.config/nvim/after/plugin/virt-column.rc.lua
+++ nvim/.config/nvim/after/plugin/virt-column.rc.lua
@@ -1,8 +1,21 @@
-local status, column = pcall(require, 'virt-column')
+local status, virtColumn = pcall(require, 'virt-column')
 if (not status) then return end
 
-column.setup({
+virtColumn.setup({
   char = "┊",
   virtcolumn = "80"
 })
+
+-- Auto insert mode when entering terminal
+local augroup_term_column = vim.api.nvim_create_augroup("Term-Virt-Column", { clear = true })
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "WinEnter", "TermOpen", "TermEnter" }, {
+  group = augroup_term_column,
+  pattern = 'term://*',
+  -- command = 'startinsert'
+  callback = function(ev)
+    virtColumn.setup_buffer { virtcolumn = '' }
+  end
+})
+
+
 
