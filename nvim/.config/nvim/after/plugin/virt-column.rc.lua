c0737bb2019babf20f5a2d07750bce42460f3df9
Umut Topuzoğlu <uloco@schiefewelt.de>
2023-07-03 14:00:37 +0200

    cleanup

--- nvim/.config/nvim/after/plugin/virt-column.rc.lua
+++ nvim/.config/nvim/after/plugin/virt-column.rc.lua
@@ -6,12 +6,10 @@ virtColumn.setup({
   virtcolumn = "80"
 })
 
--- Auto insert mode when entering terminal
 local augroup_term_column = vim.api.nvim_create_augroup("Term-Virt-Column", { clear = true })
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "WinEnter", "TermOpen", "TermEnter" }, {
   group = augroup_term_column,
   pattern = 'term://*',
-  -- command = 'startinsert'
   callback = function(ev)
     virtColumn.setup_buffer { virtcolumn = '' }
   end
