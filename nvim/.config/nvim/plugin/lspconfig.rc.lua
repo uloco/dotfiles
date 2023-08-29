ed5bc3b04b32e11b8e35de57232f2d8660e6b6ce
Umut Topuzoğlu <uloco@schiefewelt.de>
2023-08-29 15:08:01 +0200

    Auto complete json files

--- nvim/.config/nvim/plugin/lspconfig.rc.lua
+++ nvim/.config/nvim/plugin/lspconfig.rc.lua
@@ -30,3 +30,63 @@ nvim_lsp.lua_ls.setup {
     }
   }
 }
+
+local capabilitiesJson = vim.lsp.protocol.make_client_capabilities()
+capabilitiesJson.textDocument.completion.completionItem.snippetSupport = true
+
+nvim_lsp.jsonls.setup {
+  capabilities = capabilitiesJson,
+  filetypes = { "json", "jsonc" },
+  settings = {
+    json = {
+      schemas = {
+        -- Schemas https://www.schemastore.org
+        {
+          description = 'TypeScript compiler configuration file',
+          fileMatch = { 'tsconfig.json', 'tsconfig.*.json' },
+          url = 'http://json.schemastore.org/tsconfig'
+        },
+        {
+          description = 'Lerna config',
+          fileMatch = { 'lerna.json' },
+          url = 'http://json.schemastore.org/lerna'
+        },
+        {
+          description = 'Babel configuration',
+          fileMatch = { '.babelrc.json', '.babelrc', 'babel.config.json' },
+          url = 'http://json.schemastore.org/lerna'
+        },
+        {
+          description = 'ESLint config',
+          fileMatch = { '.eslintrc.json', '.eslintrc' },
+          url = 'http://json.schemastore.org/eslintrc'
+        },
+        {
+          description = 'Bucklescript config',
+          fileMatch = { 'bsconfig.json' },
+          url = 'https://bucklescript.github.io/bucklescript/docson/build-schema.json'
+        },
+        {
+          description = 'Prettier config',
+          fileMatch = { '.prettierrc', '.prettierrc.json', 'prettier.config.json' },
+          url = 'http://json.schemastore.org/prettierrc'
+        },
+        {
+          description = 'Vercel Now config',
+          fileMatch = { 'now.json' },
+          url = 'http://json.schemastore.org/now'
+        },
+        {
+          description = 'Stylelint config',
+          fileMatch = { '.stylelintrc', '.stylelintrc.json', 'stylelint.config.json' },
+          url = 'http://json.schemastore.org/stylelintrc'
+        },
+        {
+          description = "Npm package config",
+          fileMatch = { "package.json" },
+          url = "https://json.schemastore.org/package.json"
+        },
+      }
+    }
+  }
+}
