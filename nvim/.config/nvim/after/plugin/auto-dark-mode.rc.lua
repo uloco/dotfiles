036bd6b56dc6ff72bd54022a579d7e2aced3b72c
Umut Topuzoğlu <uloco@schiefewelt.de>
2023-10-13 11:06:45 +0200

    Fix auto-dark mode breaking bufferline

--- nvim/.config/nvim/after/plugin/auto-dark-mode.rc.lua
+++ nvim/.config/nvim/after/plugin/auto-dark-mode.rc.lua
@@ -6,13 +6,15 @@ local bluloco = require("bluloco")
 auto_dark_mode.setup({
   update_interval = 1000,
   set_dark_mode = function()
-    if (bluloco.config.style == "auto") then
+    local current = vim.api.nvim_get_option('background')
+    if (bluloco.config.style == "auto" and current ~= 'dark') then
       vim.api.nvim_set_option('background', 'dark')
       vim.cmd [[colorscheme bluloco]]
     end
   end,
   set_light_mode = function()
-    if (bluloco.config.style == "auto") then
+    local current = vim.api.nvim_get_option('background')
+    if (bluloco.config.style == "auto" and current ~= 'light') then
       vim.api.nvim_set_option('background', 'light')
       vim.cmd [[colorscheme bluloco]]
     end
