a128708af40b98c03eecfceecbe2c6640608e910
Umut Topuzoğlu <uloco@schiefewelt.de>
2023-11-22 17:09:48 +0100

    [wip] custom winbar

--- nvim/.config/nvim/after/plugin/bufferline.rc.lua
+++ nvim/.config/nvim/after/plugin/bufferline.rc.lua
@@ -24,3 +24,30 @@ vim.keymap.set('n', '<A-S-i>', '<cmd>BufferLineMoveNext<cr>', opt)
 vim.keymap.set('n', '<A-S-u>', '<cmd>BufferLineMovePrev<cr>', opt)
 
 vim.keymap.set('n', '<Leader>gt', ':BufferLinePick<cr>', opt)
+
+
+local function status_line()
+  local mode = "%-5{%v:lua.string.upper(v:lua.vim.fn.mode())%}"
+  local file_name = "%t"
+  local buf_nr = "[%n]"
+  -- local modified = " %#Method# %-m"
+  local modified = ""
+  local file_type = " %y"
+  local right_align = "%="
+  local line_no = "%10([%l/%L%)]"
+  local pct_thru_file = "%5p%%"
+
+  local diagnostics = vim.diagnostics.get
+  if "%-m" then
+    modified  = "%#Type# •%#Normal#"
+  end
+
+  return file_name .. modified
+  -- return string.format(
+  --   "%s %s",
+  --   file_name,
+  --   modified,
+  -- )
+end
+
+-- vim.opt.winbar = status_line()
