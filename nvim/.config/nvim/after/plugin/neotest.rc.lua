494c9e1b7525f97ef10ddd59286634685cf107d3
Umut Topuzoğlu <uloco@schiefewelt.de>
2023-11-22 17:00:30 +0100

    Add neotest commands

--- nvim/.config/nvim/after/plugin/neotest.rc.lua
+++ nvim/.config/nvim/after/plugin/neotest.rc.lua
@@ -13,3 +13,13 @@ neotest.setup({
     }),
   },
 })
+
+
+local cmd = vim.api.nvim_create_user_command
+cmd("NeotestSummary", function() require("neotest").summary.toggle() end, {})
+cmd("NeotestFile", function() require("neotest").run.run(vim.fn.expand("%")) end, {})
+cmd("Neotest", function() require("neotest").run.run(vim.fn.getcwd()) end, {})
+cmd("NeotestNearest", function() require("neotest").run.run() end, {})
+cmd("NeotestDebug", function() require("neotest").run.run({ strategy = "dap" }) end, {})
+cmd("NeotestAttach", function() require("neotest").run.attach() end, {})
+cmd("NeotestOutput", function() require("neotest").output.open() end, {})
