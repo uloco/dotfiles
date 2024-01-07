9e5bbf7c7f4331690a3363fda443bed5393db16a
Umut Topuzoğlu <uloco@schiefewelt.de>
2024-01-07 19:36:59 +0100

    remove obsolete plugins

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -154,13 +154,6 @@ packer.startup(function(use)
 
   -- search and replace
   use { 'windwp/nvim-spectre' }
-  use({
-    "ray-x/sad.nvim",
-    requires = { "ray-x/guihua.lua", run = "cd lua/fzy && make" },
-    config = function()
-      require("sad").setup {}
-    end,
-  })
 
   -- tree explorer
   use {
