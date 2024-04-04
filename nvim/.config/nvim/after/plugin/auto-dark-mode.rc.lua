2ec7ff1e01e1306afe16c1be01530047eea22f08
Umut Topuzoğlu <uloco@schiefewelt.de>
2024-04-04 23:00:29 +0200

    Disable broken auto dark mode

--- nvim/.config/nvim/after/plugin/auto-dark-mode.rc.lua
+++ nvim/.config/nvim/after/plugin/auto-dark-mode.rc.lua
@@ -1,21 +1,21 @@
-local auto_dark_mode = require('auto-dark-mode')
+-- local auto_dark_mode = require('auto-dark-mode')
 
-auto_dark_mode.setup({
-  update_interval = 1000,
-  set_dark_mode = function()
-    local current = vim.api.nvim_get_option('background')
-    if (current == 'light') then
-      -- vim.api.nvim_set_option('background', 'dark')
-      vim.o.background = 'dark'
-      vim.cmd [[colorscheme bluloco]]
-    end
-  end,
-  set_light_mode = function()
-    local current = vim.api.nvim_get_option('background')
-    if (current == 'dark') then
-      -- vim.api.nvim_set_option('background', 'light')
-      vim.o.background = 'light'
-      vim.cmd [[colorscheme bluloco]]
-    end
-  end,
-})
+-- auto_dark_mode.setup({
+--   update_interval = 1000,
+--   set_dark_mode = function()
+--     local current = vim.api.nvim_get_option('background')
+--     if (current == 'light') then
+--       -- vim.api.nvim_set_option('background', 'dark')
+--       vim.o.background = 'dark'
+--       vim.cmd [[colorscheme bluloco]]
+--     end
+--   end,
+--   set_light_mode = function()
+--     local current = vim.api.nvim_get_option('background')
+--     if (current == 'dark') then
+--       -- vim.api.nvim_set_option('background', 'light')
+--       vim.o.background = 'light'
+--       vim.cmd [[colorscheme bluloco]]
+--     end
+--   end,
+-- })
