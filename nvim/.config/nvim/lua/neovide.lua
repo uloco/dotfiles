ef014a2520621f2fafe95aa63b0541a2901d3f91
Umut Topuzoğlu <uloco@schiefewelt.de>
2024-04-24 10:33:46 +0200

    neovide meta alt key setting comment

--- nvim/.config/nvim/lua/neovide.lua
+++ nvim/.config/nvim/lua/neovide.lua
@@ -2,4 +2,7 @@ if vim.g.neovide then
   -- Put anything you want to happen only in Neovide here
   vim.g.neovide_input_macos_alt_is_meta = false
 
+  -- option merged but not released yet
+  -- vim.g.neovide_input_macos_alt_is_meta = 'only_left'
+
 end
