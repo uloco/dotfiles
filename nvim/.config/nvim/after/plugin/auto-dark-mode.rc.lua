2740724519eac9e8ee1c8c67292a55995f0bfe2f
Umut Topuzoğlu <uloco@schiefewelt.de>
2024-06-03 16:32:01 +0200

    Fix auto dark mode setting

--- nvim/.config/nvim/after/plugin/auto-dark-mode.rc.lua
+++ nvim/.config/nvim/after/plugin/auto-dark-mode.rc.lua
@@ -1,21 +1,19 @@
--- local auto_dark_mode = require('auto-dark-mode')
+local auto_dark_mode = require('auto-dark-mode')
 
--- auto_dark_mode.setup({
---   update_interval = 1000,
---   set_dark_mode = function()
---     local current = vim.api.nvim_get_option('background')
---     if (current == 'light') then
---       -- vim.api.nvim_set_option('background', 'dark')
---       vim.o.background = 'dark'
---       vim.cmd [[colorscheme bluloco]]
---     end
---   end,
---   set_light_mode = function()
---     local current = vim.api.nvim_get_option('background')
---     if (current == 'dark') then
---       -- vim.api.nvim_set_option('background', 'light')
---       vim.o.background = 'light'
---       vim.cmd [[colorscheme bluloco]]
---     end
---   end,
--- })
+local function isAuto()
+  return require('bluloco').config.style == 'auto'
+end
+
+auto_dark_mode.setup({
+  update_interval = 1000,
+  set_dark_mode = function()
+    if isAuto() then
+      vim.o.background = 'dark'
+    end
+  end,
+  set_light_mode = function()
+    if isAuto() then
+      vim.o.background = 'light'
+    end
+  end
+})
