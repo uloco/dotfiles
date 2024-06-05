a3d007789affb6159719f06368bde3c340d73fa4
Umut Topuzoğlu <uloco@schiefewelt.de>
2024-06-06 00:21:22 +0200

    Add rainbow config

--- nvim/.config/nvim/after/plugin/indent-blankline.rc.lua
+++ nvim/.config/nvim/after/plugin/indent-blankline.rc.lua
@@ -1,10 +1,20 @@
+local highlight = {
+  "RainbowRed",
+  "RainbowYellow",
+  "RainbowBlue",
+  "RainbowOrange",
+  "RainbowGreen",
+  "RainbowViolet",
+  "RainbowCyan",
+}
+
 require("ibl").setup({
   indent = {
-    char = "▏"
+    char = "▏",
+    -- highlight = highlight -- enables rainbow highlight
   },
   scope = {
     show_start = false,
     show_end = false,
-    -- highlight = "Label"
-  }
+  },
 })
