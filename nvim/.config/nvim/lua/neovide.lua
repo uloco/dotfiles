90476a391289a3df20b028941550a610ca4bd0b1
Umut Topuzoğlu <uloco@schiefewelt.de>
2024-06-27 13:41:42 +0200

    Fix neovide font

--- nvim/.config/nvim/lua/neovide.lua
+++ nvim/.config/nvim/lua/neovide.lua
@@ -1,5 +1,5 @@
 if vim.g.neovide then
-  vim.o.guifont = "Iosevka"
+  vim.o.guifont = "IosevkaTerm Nerd Font Mono:h16"
   vim.g.neovide_transparency = 0.97
   vim.g.neovide_cursor_animation_length = 0
   vim.g.neovide_scroll_animation_length = 0
