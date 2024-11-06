0639962d9317db1fff4b3f58fa4fe93afba030ee
Umut Topuzoğlu <uloco@schiefewelt.de>
2024-11-06 13:49:46 +0100

    Disable auto format

--- nvim/.config/nvim/after/plugin/null-ls.rc.lua
+++ nvim/.config/nvim/after/plugin/null-ls.rc.lua
@@ -19,19 +19,19 @@ null_ls.setup {
   },
   on_attach = function(client)
     if client.server_capabilities.documentFormattingProvider then
-      -- auto format
-      vim.api.nvim_clear_autocmds { buffer = 0, group = augroup_format }
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup_format,
-        buffer = 0,
-        callback = function()
-          vim.lsp.buf.format({
-            async = false,
-            -- Never request typescript-language-server for formatting
-            filter = function(c) return c.name ~= "tsserver" end
-          })
-        end
-      })
+      --   -- auto format
+      --   vim.api.nvim_clear_autocmds { buffer = 0, group = augroup_format }
+      --   vim.api.nvim_create_autocmd("BufWritePre", {
+      --     group = augroup_format,
+      --     buffer = 0,
+      --     callback = function()
+      --       vim.lsp.buf.format({
+      --         async = false,
+      --         -- Never request typescript-language-server for formatting
+      --         filter = function(c) return c.name ~= "tsserver" end
+      --       })
+      --     end
+      --   })
     end
   end,
 
