95359888dce29a346034995742b9d0eae17eb102
Umut Topuzoğlu <uloco@schiefewelt.de>
2025-01-10 14:12:23 +0100

    Cleanup

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -206,19 +206,6 @@ packer.startup(function(use)
     }
   }
 
-  -- use {
-  --   "ggandor/leap.nvim",
-  --   config = function()
-  --     local leap = require('leap')
-  --     leap.create_default_mappings()
-  --     leap.opts.safe_labels = {}
-
-  --     vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
-  --     vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
-  --     vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap-from-window)')
-  --   end
-  -- }
-
   use { "ibhagwan/fzf-lua",
     requires = { "nvim-tree/nvim-web-devicons" },
     config = function()
