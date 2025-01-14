dae472f0a5be5e5111122c1a64bd69adb75048a1
Umut Topuzoğlu <uloco@schiefewelt.de>
2025-01-14 18:00:39 +0100

    Add codecompanion (disabled)

---
+++ nvim/.config/nvim/after/plugin/codecompanion.rc.lua
@@ -0,0 +1,14 @@
+local status, codecompanion = pcall(require, 'codecompanion')
+if (not status) then return end
+
+codecompanion.setup({})
+-- codecompanion.setup({
+--   strategies = {
+--     chat = {
+--       adapter = "copilot",
+--     },
+--     inline = {
+--       adapter = "copilot",
+--     },
+--   },
+-- })
