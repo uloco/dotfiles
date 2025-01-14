2a358d1e4e6429f06d4defbf4d22f554b6ee1051
Umut Topuzoğlu <uloco@schiefewelt.de>
2025-01-14 17:59:57 +0100

    Add reverse search cmp

--- nvim/.config/nvim/after/plugin/cmp.rc.lua
+++ nvim/.config/nvim/after/plugin/cmp.rc.lua
@@ -80,7 +80,7 @@ cmp.setup({
 })
 
 -- Use buffer source for `/`
-cmp.setup.cmdline('/', {
+cmp.setup.cmdline({ '/', '?' }, {
   mapping = cmp.mapping.preset.cmdline(),
   sources = {
     { name = 'buffer' }
