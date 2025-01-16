167e227aab6de2b744f471ae64174c1bbc863490
Umut Topuzoğlu <uloco@schiefewelt.de>
2025-01-16 19:59:54 +0100

    accept copilot on c-i

--- nvim/.config/nvim/after/plugin/copilot.rc.lua
+++ nvim/.config/nvim/after/plugin/copilot.rc.lua
@@ -23,7 +23,7 @@ copilot.setup({
     hide_during_completion = true,
     debounce = 75,
     keymap = {
-      accept = "<Tab>",
+      accept = "<C-i>",
       accept_word = false,
       accept_line = false,
       next = "<C-o>",
