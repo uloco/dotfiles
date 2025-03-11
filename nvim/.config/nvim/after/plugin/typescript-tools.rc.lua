a4e49e89cd0cc6230c0ad7c38a9c81f7327b9742
Umut Topuzoğlu <uloco@schiefewelt.de>
2025-03-11 15:46:45 +0100

    Always use relative file import paths ts

--- nvim/.config/nvim/after/plugin/typescript-tools.rc.lua
+++ nvim/.config/nvim/after/plugin/typescript-tools.rc.lua
@@ -1,5 +1,8 @@
 require("typescript-tools").setup {
   settings = {
     expose_as_code_action = "all",
+    tsserver_file_preferences = {
+      importModuleSpecifierPreference = "non-relative",
+    },
   }
 }
