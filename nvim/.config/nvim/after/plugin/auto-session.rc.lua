fa1a44cea394e1ae4d4500c6863d2d6447bb3f39
Umut Topuzoğlu <uloco@schiefewelt.de>
2025-09-17 11:56:52 +0200

    Update auto-session config

--- nvim/.config/nvim/after/plugin/auto-session.rc.lua
+++ nvim/.config/nvim/after/plugin/auto-session.rc.lua
@@ -1,30 +1,29 @@
 local status, auto_session = pcall(require, 'auto-session')
 if (not status) then return end
 
--- vim.api.nvim_create_autocmd({ 'BufEnter' }, {
---   pattern = 'NvimTree*',
---   callback = function()
---     local api = require('nvim-tree.api')
---     local view = require('nvim-tree.view')
---     if not view.is_visible() then
---       api.tree.open()
---     end
---   end,
--- })
-
-function _G.close_all_floating_wins()
-  for _, win in ipairs(vim.api.nvim_list_wins()) do
-    local config = vim.api.nvim_win_get_config(win)
-    if config.relative ~= '' then
-      vim.api.nvim_win_close(win, false)
-    end
-  end
-end
+vim.opt.sessionoptions = {
+  "blank",
+  "buffers",
+  "curdir",
+  "folds",
+  "help",
+  "tabpages",
+  "winsize",
+  "winpos",
+  "terminal",
+  "localoptions",
+}
 
 auto_session.setup {
   log_level = "error",
   auto_session_suppress_dirs = { "~/", "~/Source", "~/Downloads", "/" },
-  pre_save_cmds = { _G.close_all_floating_wins },
+  bypass_save_filetypes = { "alpha", "dashboard", "snacks_dashboard" }, -- or whatever dashboard you use
+  purge_after_minutes = 43200,
+  legacy_cmds = false,
+  -- cwd_change_handling = true,
+  -- git_use_branch_name = true,
+  -- git_auto_restore_on_branch_change = true,
+
 }
 
-vim.keymap.set({ "n" }, "<leader>lp", "<cmd>SessionSearch<cr>")
+vim.keymap.set({ "n" }, "<leader>lp", "<cmd>AutoSession search<cr>")
