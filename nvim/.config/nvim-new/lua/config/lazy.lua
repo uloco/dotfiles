345e3c66a79a66eca680d455dc6ca9b8330f3cc3
Umut Topuzoğlu <uloco@schiefewelt.de>
2025-09-19 11:03:46 +0200

    Don't show lazy notifiy on change

--- nvim/.config/nvim-new/lua/config/lazy.lua
+++ nvim/.config/nvim-new/lua/config/lazy.lua
@@ -26,9 +26,10 @@ require("lazy").setup({
 		-- import your plugins
 		{ import = "plugins" },
 	},
+	change_detection = { notify = false },
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
-	install = { colorscheme = { "habamax" } },
+	install = { colorscheme = { "bluloco" } },
 	-- automatically check for plugin updates
 	checker = { enabled = true },
 })
