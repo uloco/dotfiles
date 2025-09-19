bf4d956aa3dc1437613e5d006622ba0f6160c111
Umut Topuzoğlu <uloco@schiefewelt.de>
2025-09-19 11:04:20 +0200

    Add snacks WIP

---
+++ nvim/.config/nvim-new/lua/plugins/snacks.lua
@@ -0,0 +1,75 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	---@type snacks.Config
+	opts = {
+		bufdelete = { enabled = true },
+		-- dashboard = { enabled = true },
+		-- explorer = { enabled = true },
+		-- image = { enabled = true },
+		indent = { enabled = true },
+		input = { enabled = true },
+		-- lazygit = { enabled = true },
+		picker = {
+			enabled = true,
+			sources = {
+				explorer = {
+					hidden = true,
+					-- your explorer picker configuration comes here
+					-- or leave it empty to use the default settings
+				},
+			},
+		},
+		-- quickfile = { enabled = true },
+		-- scope = { enabled = true },
+		-- scroll = { enabled = true },
+		-- statuscolumn = { enabled = true },
+		words = { enabled = true },
+		styles = {
+			notification = {
+				-- wo = { wrap = true } -- Wrap notifications
+			},
+		},
+	},
+	keys = {
+		{
+			"<A-q>",
+			function()
+				Snacks.bufdelete.delete()
+			end,
+			mode = { "n", "i", "x", "t" },
+			desc = "Delete Buffer",
+		},
+		{
+			"<A-S-q>",
+			function()
+				Snacks.bufdelete.all()
+			end,
+			mode = { "n", "i", "x", "t" },
+			desc = "Delete All Buffers",
+		},
+		{
+			"<A-S-o>",
+			function()
+				Snacks.bufdelete.other()
+			end,
+			mode = { "n", "i", "x", "t" },
+			desc = "Delete Other Buffers",
+		},
+		-- {
+		-- 	"<leader>b",
+		-- 	function()
+		-- 		Snacks.explorer.open()
+		-- 	end,
+		-- 	desc = "Toggle Explorer",
+		-- },
+		-- {
+		-- 	"<leader>ff",
+		-- 	function()
+		-- 		Snacks.explorer.reveal()
+		-- 	end,
+		-- 	desc = "Find File in Explorer",
+		-- },
+	},
+}
