93f01253ee3aa35b62d91589e146477f0de8fedf
Umut Topuzoğlu <uloco@schiefewelt.de>
2025-09-24 16:22:42 +0200

    oil

--- nvim/.config/nvim-new/lua/plugins/oil.lua
+++ nvim/.config/nvim-new/lua/plugins/oil.lua
@@ -2,7 +2,19 @@ return {
 	"stevearc/oil.nvim",
 	---@module 'oil'
 	---@type oil.SetupOpts
-	opts = {},
+	opts = {
+		float = {
+			max_width = 0.5,
+			win_options = {
+				winblend = 15,
+			},
+		},
+		keys = {
+			["<C-c>"] = "actions.close",
+			-- ["<C-d>"] = "actions.preview_scroll_down",
+			-- ["<C-u>"] = "actions.preview_scroll_up",
+		},
+	},
 	dependencies = { "nvim-mini/mini.nvim" },
 	lazy = false,
 	keys = {
