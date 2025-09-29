e832141fa26943dac8e8e279d2a4d5008ce97425
Umut Topuzoğlu <uloco@schiefewelt.de>
2025-09-29 09:37:12 +0200

    tree config

--- nvim/.config/nvim-new/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim-new/lua/plugins/neo-tree.lua
@@ -7,6 +7,7 @@ return {
 			"MunifTanjim/nui.nvim",
 			"nvim-mini/mini.nvim",
 		},
+		enabled = true,
 		lazy = false, -- neo-tree will lazily load itself
 		opts = {
 			default_component_configs = {
@@ -43,11 +44,7 @@ return {
 			},
 			window = {
 				mappings = {
-					-- ["<space>"] = {
-					-- 	"toggle_node",
-					-- 	nowait = true, -- disable `nowait` if you have existing combos starting with this char that you want to use
-					-- },
-					-- ["<leader>b"] = "close_window",
+					["/"] = "none", -- disable fuzzy filter
 				},
 			},
 			filesystem = {
@@ -58,14 +55,37 @@ return {
 					},
 				},
 				follow_current_file = {
-					enabled = true, -- This will find and focus the file in the active buffer every time
+					enabled = false, -- This will find and focus the file in the active buffer every time
 					--               -- the current file is changed while the tree is open.
 					leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
 				},
 			},
+			event_handlers = {
+				{
+					event = "neo_tree_buffer_enter",
+					handler = function(arg)
+						vim.cmd([[
+          setlocal number
+          setlocal relativenumber
+          ]])
+					end,
+				},
+			},
 		},
+		config = function(_, opts)
+			local function on_move(data)
+				Snacks.rename.on_rename_file(data.source, data.destination)
+			end
+			local events = require("neo-tree.events")
+			opts.event_handlers = opts.event_handlers or {}
+			vim.list_extend(opts.event_handlers, {
+				{ event = events.FILE_MOVED, handler = on_move },
+				{ event = events.FILE_RENAMED, handler = on_move },
+			})
+			require("neo-tree").setup(opts)
+		end,
 		keys = {
-			{ "<leader>b", "<cmd>Neotree toggle<cr>", desc = "Toggle NeoTree" },
+			{ "<leader>B", "<cmd>Neotree toggle<cr>", desc = "Toggle NeoTree" },
 			-- { "<leader>bb", "<cmd>Neotree buffers<cr>", desc = "Toggle NeoTree" },
 			{ "<leader>ff", "<cmd>Neotree reveal<cr>", desc = "Find File in NeoTree" },
 		},
