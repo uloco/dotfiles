79281467794ab72e3c4577708e4b53068c2083dd
Umut Topuzoğlu <uloco@schiefewelt.de>
2025-10-02 11:32:54 +0200

    Add mini move

--- nvim/.config/nvim-new/lua/plugins/mini.lua
+++ nvim/.config/nvim-new/lua/plugins/mini.lua
@@ -4,5 +4,25 @@ return {
 	config = function()
 		require("mini.icons").setup()
 		MiniIcons.mock_nvim_web_devicons()
+
+		require("mini.move").setup({
+			-- Module mappings. Use `''` (empty string) to disable one.
+			mappings = {
+				-- Move visual selection in Visual mode.
+				left = "<C-h>",
+				right = "<C-l>",
+				down = "<C-j>",
+				up = "<C-k>",
+
+				-- Move current line in Normal mode
+				line_left = "<C-h>",
+				line_right = "<C-l>",
+				line_down = "<C-j>",
+				line_up = "<C-k>",
+			},
+			options = {
+				reindent_linewise = false,
+			},
+		})
 	end,
 }
