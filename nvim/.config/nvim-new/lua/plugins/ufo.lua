1712879a8a30d11a22f632022ba4a73cc517bffe
Umut Topuzoğlu <uloco@schiefewelt.de>
2025-10-02 10:14:48 +0200

    Add folding with ufo

---
+++ nvim/.config/nvim-new/lua/plugins/ufo.lua
@@ -0,0 +1,28 @@
+return {
+	"kevinhwang91/nvim-ufo",
+	event = "VeryLazy",
+	dependencies = {
+		"kevinhwang91/promise-async",
+	},
+	opts = {
+		provider_selector = function()
+			return { "lsp", "indent" }
+		end,
+	},
+	keys = {
+		{
+			"zR",
+			function()
+				require("ufo").openAllFolds()
+			end,
+			desc = "Open all folds",
+		},
+		{
+			"zM",
+			function()
+				require("ufo").closeAllFolds()
+			end,
+			desc = "Close all folds",
+		},
+	},
+}
