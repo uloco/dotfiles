a4ec0d042c67b139a726dca63c613fe6a07978fe
Umut Topuzoğlu <uloco@schiefewelt.de>
2025-10-06 23:12:06 +0200

    Add mini surround

--- nvim/.config/nvim-new/lua/plugins/mini.lua
+++ nvim/.config/nvim-new/lua/plugins/mini.lua
@@ -14,18 +14,7 @@ return {
 		require("mini.icons").setup()
 		MiniIcons.mock_nvim_web_devicons()
 
-		-- require("mini.surround").setup({
-		-- 	mappings = {
-		-- 		add = "gsa", -- Add surrounding in Normal and Visual modes
-		-- 		delete = "gsd", -- Delete surrounding
-		-- 		find = "gsf", -- Find surrounding (to the right)
-		-- 		find_left = "gsF", -- Find surrounding (to the left)
-		-- 		highlight = "gsh", -- Highlight surrounding
-		-- 		replace = "gsr", -- Replace surrounding
-		-- 		update_n_lines = "gsn", -- Update `n_lines`
-		-- 	},
-		-- })
-		-- require("mini.pairs").setup()
+		require("mini.surround").setup()
 
 		require("mini.files").setup()
 
