d25d84c6b3bd4ce4ebdc6cdb47cddb4b5e3fecb8
Umut Topuzoğlu <uloco@schiefewelt.de>
2026-02-02 00:00:55 +0100

    Disable typescript-tools

--- nvim/.config/nvim-new/lua/plugins/typescript-tools.lua
+++ nvim/.config/nvim-new/lua/plugins/typescript-tools.lua
@@ -1,6 +1,7 @@
 return {
 	"pmizio/typescript-tools.nvim",
 	dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  enabled = false,
 	opts = {
 		settings = {
 			expose_as_code_action = "all",
