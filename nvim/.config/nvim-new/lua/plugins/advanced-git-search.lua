65f61654c33acf828ba6e9265ed8a3d69c46d846
Umut Topuzoğlu <uloco@schiefewelt.de>
2026-02-02 01:25:52 +0100

    Remove unused

--- nvim/.config/nvim-new/lua/plugins/advanced-git-search.lua
+++ nvim/.config/nvim-new/lua/plugins/advanced-git-search.lua
@@ -1,6 +1,7 @@
 -- not yet configured fully
 return {
 	"aaronhallaert/advanced-git-search.nvim",
+  enabled = false,
 	cmd = { "AdvancedGitSearch" },
 	config = function()
 		require("advanced_git_search.snacks").setup({
