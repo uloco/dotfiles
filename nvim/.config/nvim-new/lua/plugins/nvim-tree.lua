966f091d345086138df8cd1986c98026ba1eb1e5
Umut Topuzoğlu <uloco@schiefewelt.de>
2026-02-02 01:23:31 +0100

    Snacks rename does not work

--- nvim/.config/nvim-new/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim-new/lua/plugins/nvim-tree.lua
@@ -8,23 +8,6 @@ return {
 			relativenumber = true,
 		},
 	},
-	config = function(plugin, opts)
-		require("nvim-tree").setup(opts)
-		-- rename file handler
-		local prev = { new_name = "", old_name = "" } -- Prevents duplicate events
-		vim.api.nvim_create_autocmd("User", {
-			pattern = "NvimTreeSetup",
-			callback = function()
-				local events = require("nvim-tree.api").events
-				events.subscribe(events.Event.NodeRenamed, function(data)
-					if prev.new_name ~= data.new_name or prev.old_name ~= data.old_name then
-						data = data
-						Snacks.rename.on_rename_file(data.old_name, data.new_name)
-					end
-				end)
-			end,
-		})
-	end,
 	keys = {
 		{ "<leader>b", "<cmd>NvimTreeToggle<cr>", desc = "Toggle Nvim Tree" },
 		{ "<leader>ff", "<cmd>NvimTreeFindFile<cr>", desc = "Find File in Nvim Tree" },
