94abc5d71972d2db3520c4590029167d873ddfad
Umut Topuzoğlu <uloco@schiefewelt.de>
2026-02-03 00:16:46 +0100

    Cleanup

--- nvim/.config/nvim-new/lua/plugins/mini.lua
+++ nvim/.config/nvim-new/lua/plugins/mini.lua
@@ -1,12 +1,3 @@
-vim.api.nvim_create_autocmd("User", {
-	pattern = "MiniFilesActionRename",
-	callback = function(event)
-		Snacks.rename.on_rename_file(event.data.from, event.data.to)
-		-- save all open buffrs after rename
-		vim.cmd("wall")
-	end,
-})
-
 return {
 	"nvim-mini/mini.nvim",
 	lazy = false,
