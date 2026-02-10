d89ef1233f5110b6fc50bb6fda5802e33e750bc0
Umut Topuzoğlu <uloco@schiefewelt.de>
2026-02-10 21:09:22 +0100

    Use local bluloco

--- nvim/.config/nvim-new/lua/plugins/bluloco.lua
+++ nvim/.config/nvim-new/lua/plugins/bluloco.lua
@@ -1,6 +1,7 @@
 -- the colorscheme should be available when starting Neovim
 return {
 	"uloco/bluloco.nvim",
+  dir = "~/Source/neovim/bluloco.nvim",
 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
 	priority = 1000, -- make sure to load this before all the other start plugins
 	dependencies = { "rktjmp/lush.nvim" },
