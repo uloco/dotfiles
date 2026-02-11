3b53a1c82fbd262fa56698f9337328869b4bd49e
Umut Topuzoğlu <uloco@schiefewelt.de>
2026-02-12 00:31:31 +0100

    Add scrollbar

--- nvim/.config/nvim-new/lua/plugins/scrollbar.lua
+++ nvim/.config/nvim-new/lua/plugins/scrollbar.lua
@@ -1,7 +1,11 @@
 return {
-	"petertriho/nvim-scrollbar",
-	config = function()
-		require("scrollbar").setup(opts)
-		require("scrollbar.handlers.gitsigns").setup()
-	end,
+	{
+		"petertriho/nvim-scrollbar",
+		event = "BufReadPost",
+		dependencies = { "lewis6991/gitsigns.nvim" },
+		config = function()
+			require("scrollbar").setup()
+			require("scrollbar.handlers.gitsigns").setup()
+		end,
+	},
 }
