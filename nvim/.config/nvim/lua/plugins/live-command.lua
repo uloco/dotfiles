46e1b08d3a3bbd9941acab30312b08cb4012a98e
Umut Topuzoğlu <uloco@schiefewelt.de>
2026-02-13 03:04:35 +0100

    cleanup

--- nvim/.config/nvim/lua/plugins/live-command.lua
+++ nvim/.config/nvim/lua/plugins/live-command.lua
@@ -2,13 +2,11 @@ return {
 	{
 		"smjonas/live-command.nvim",
 		event = "CmdlineEnter",
-		config = function()
-			require("live-command").setup({
-				commands = {
-					Norm = { cmd = "norm" },
-					Glob = { cmd = "g", hl_range = { 1, -1, kind = "visible" } },
-				},
-			})
-		end,
+		opts = {
+			commands = {
+				Norm = { cmd = "norm" },
+				Glob = { cmd = "g", hl_range = { 1, -1, kind = "visible" } },
+			},
+		},
 	},
 }
