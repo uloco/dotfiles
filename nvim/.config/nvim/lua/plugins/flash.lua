acfdc4d59be00cc5783aac5c16fa15acce1aee91
Umut Topuzoğlu <uloco@schiefewelt.de>
2026-02-24 00:30:46 +0100

    disable flash

--- nvim/.config/nvim/lua/plugins/flash.lua
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -1,5 +1,6 @@
 return {
 	"folke/flash.nvim",
+	enabled = false,
 	event = "VeryLazy",
 	opts = {
 		modes = {
@@ -7,11 +8,39 @@ return {
 			char = { enabled = true },
 		},
 	},
-	-- stylua: ignore
 	keys = {
-		{ "gs", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-		{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-		{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+		{
+			"gs",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").jump()
+			end,
+			desc = "Flash",
+		},
+		{
+			"r",
+			mode = "o",
+			function()
+				require("flash").remote()
+			end,
+			desc = "Remote Flash",
+		},
+		{
+			"R",
+			mode = { "o", "x" },
+			function()
+				require("flash").treesitter_search()
+			end,
+			desc = "Treesitter Search",
+		},
+		{
+			"<c-s>",
+			mode = { "c" },
+			function()
+				require("flash").toggle()
+			end,
+			desc = "Toggle Flash Search",
+		},
 		{
 			"<C-Space>",
 			mode = { "n", "x", "o" },
