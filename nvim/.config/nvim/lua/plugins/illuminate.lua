995fa70489aafea040db266e9fa34e21adf07d1b
Umut Topuzoğlu <uloco@schiefewelt.de>
2026-04-03 21:20:21 +0200

    Fix illuminate in terminals and sidekick

--- nvim/.config/nvim/lua/plugins/illuminate.lua
+++ nvim/.config/nvim/lua/plugins/illuminate.lua
@@ -1,19 +1,25 @@
 return {
-	"RRethy/vim-illuminate",
-	keys = {
-		{
-			"<A-n>",
-			function()
-				require("illuminate").goto_next_reference()
-			end,
-			desc = "Jump to next reference",
-		},
-		{
-			"<A-S-n>",
-			function()
-				require("illuminate").goto_prev_reference()
-			end,
-			desc = "Jump to previous reference",
-		},
-	},
+  "RRethy/vim-illuminate",
+  opts = {
+    filetypes_denylist = {
+      "snacks_terminal",
+      "sidekick_terminal",
+    },
+  },
+  keys = {
+    {
+      "<A-n>",
+      function()
+        require("illuminate").goto_next_reference()
+      end,
+      desc = "Jump to next reference",
+    },
+    {
+      "<A-S-n>",
+      function()
+        require("illuminate").goto_prev_reference()
+      end,
+      desc = "Jump to previous reference",
+    },
+  },
 }
