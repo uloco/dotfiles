5b460415a416db9f4f83f68017abda6c4a796c96
Umut Topuzoğlu <uloco@schiefewelt.de>
2026-04-16 09:17:10 +0200

    use autopairs

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -28,12 +28,11 @@ return {
 			},
 		})
 
-		require("mini.pairs").setup()
-
 		require("mini.comment").setup({
 			options = {
 				custom_commentstring = function()
-					return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+					return require("ts_context_commentstring.internal").calculate_commentstring()
+						or vim.bo.commentstring
 				end,
 			},
 		})
