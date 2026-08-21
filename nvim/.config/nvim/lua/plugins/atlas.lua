a1cfc0a6cf84e31f95b5ed2b529d48a781f2b8df
Umut Topuzoğlu <uloco@schiefewelt.de>
2026-08-21 14:58:26 +0200

    Add atlas provider github

--- nvim/.config/nvim/lua/plugins/atlas.lua
+++ nvim/.config/nvim/lua/plugins/atlas.lua
@@ -4,6 +4,46 @@ return {
 		"MeanderingProgrammer/render-markdown.nvim",
 		"esmuellert/codediff.nvim",
 	},
-	-- See Configuration below
-	opts = {},
+	opts = {
+		pulls = {
+			providers = {
+				github = {
+					views = {
+						{
+							name = "My PRs",
+							key = "1",
+							layout = "plain",
+							search = "author:@me sort:updated-desc",
+						},
+						{
+							name = "Review Requested",
+							key = "2",
+							layout = "plain",
+							search = "review-requested:@me sort:updated-desc",
+						},
+					},
+				},
+			},
+		},
+		issues = {
+			providers = {
+				github = {
+					views = {
+						{
+							name = "Assigned",
+							key = "1",
+							layout = "plain",
+							search = "assignee:@me is:open",
+						},
+						{
+							name = "Created",
+							key = "2",
+							layout = "plain",
+							search = "author:@me is:open",
+						},
+					},
+				},
+			},
+		},
+	},
 }
