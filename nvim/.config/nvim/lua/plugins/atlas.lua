780b409f6f21344cea9d4c88c050e3864e4e112b
Umut Topuzoğlu <uloco@schiefewelt.de>
2026-08-21 13:22:11 +0200

    Add atlas and render-markdown

---
+++ nvim/.config/nvim/lua/plugins/atlas.lua
@@ -0,0 +1,9 @@
+return {
+	"emrearmagan/atlas.nvim",
+	dependencies = {
+		"MeanderingProgrammer/render-markdown.nvim",
+		"esmuellert/codediff.nvim",
+	},
+	-- See Configuration below
+	opts = {},
+}
