d16b16bfeeb11eeabd26820c8d624b8241a8b727
Umut Topuzoğlu <uloco@schiefewelt.de>
2026-09-08 13:16:10 +0200

    Add atlas gitlab config tonies

--- nvim/.config/nvim/lua/plugins/atlas.lua
+++ nvim/.config/nvim/lua/plugins/atlas.lua
@@ -7,8 +7,15 @@ return {
 	opts = {
 		providers = {
 			github = {},
+			gitlab = {
+				base_url = "https://gitlab.boxine.de",
+				token = vim.env.TONIES_GITLAB_TOKEN,
+			},
 		},
 		pulls = {
+			default_merge_method = "squash", -- "merge" or "squash"
+			default_delete_branch = true,
+			git_transport = "ssh",
 			github = {
 				views = {
 					{
@@ -22,6 +29,22 @@ return {
 						key = "2",
 						layout = "plain",
 						search = "review-requested:@me sort:updated-desc",
+					},
+				},
+			},
+			gitlab = {
+				views = {
+					{
+						name = "My MRs",
+						key = "1",
+						layout = "plain",
+						scope = "assigned_to_me",
+					},
+					{
+						name = "Reviewing",
+						key = "2",
+						layout = "plain",
+						scope = "reviews_for_me",
 					},
 				},
 			},
@@ -40,6 +63,22 @@ return {
 						key = "2",
 						layout = "plain",
 						search = "author:@me is:open",
+					},
+				},
+			},
+			gitlab = {
+				views = {
+					{
+						name = "Assigned",
+						key = "1",
+						scope = "assigned_to_me",
+						state = "opened",
+					},
+					{
+						name = "Created",
+						key = "2",
+						scope = "created_by_me",
+						state = "opened",
 					},
 				},
 			},
